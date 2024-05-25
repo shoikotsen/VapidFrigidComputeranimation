3276a30146c760ef938be79dbf291476fbd7cc63
shoikotsen <shoikot@usf.edu>
2024-05-25 05:58:41 +0000

    Initial commit

---
+++ Lyrics.cs
@@ -0,0 +1,32 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+// Unit Test #1
+        Console.WriteLine("Oh, well imagine /");
+        Console.WriteLine("As I'm pacing the pews in a church corridor /");
+        Console.WriteLine("And I can't help but to hear /");
+        Console.WriteLine("No, I can't help but to hear an exchanging of words...");
+
+        // Unit Test #2
+        Console.WriteLine("\nOh-oh-oh-oh-oh,");
+        Console.WriteLine("");
+        Console.WriteLine("oh-oh-oh-oh,");
+        Console.WriteLine("");
+        Console.WriteLine("oh-oh-oh /");
+        Console.WriteLine("Caught in a bad romance...");
+
+        // Unit Test #3
+        Console.WriteLine("\nComing out of my cage");
+        Console.WriteLine("And I've been doing just fine");
+        Console.WriteLine("Gotta gotta be down");
+        Console.WriteLine("Because I want it all");
+
+        // Unit Test #4
+        Console.WriteLine("\nNow, this is a story all about how");
+        Console.WriteLine("My life got flipped-turned upside down");
+        Console.WriteLine("And I'd like to take a minute");
+        Console.WriteLine("Just sit right there");
+        Console.WriteLine("I'll tell you how I became the prince of a town called Bel-Air");
+    }
+}
