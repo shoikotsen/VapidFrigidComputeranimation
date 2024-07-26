1fbfb14238e5fb68309111809424eb09553cb93f
shoikotsen <shoikot@usf.edu>
2024-07-26 04:06:13 +0000

    comments

--- Lyrics.cs
+++ Lyrics.cs
@@ -3,12 +3,14 @@ using System;
 class Program {
   public static void Main (string[] args) {
 // Unit Test #1
+    // Printing the first set of lyrics to the console
         Console.WriteLine("Oh, well imagine /");
         Console.WriteLine("As I'm pacing the pews in a church corridor /");
         Console.WriteLine("And I can't help but to hear /");
         Console.WriteLine("No, I can't help but to hear an exchanging of words...");
 
         // Unit Test #2
+    // Printing the second set of lyrics to the console with some blank lines
         Console.WriteLine("\nOh-oh-oh-oh-oh,");
         Console.WriteLine("");
         Console.WriteLine("oh-oh-oh-oh,");
@@ -17,12 +19,14 @@ class Program {
         Console.WriteLine("Caught in a bad romance...");
 
         // Unit Test #3
+    // Printing the third set of lyrics to the console
         Console.WriteLine("\nComing out of my cage");
         Console.WriteLine("And I've been doing just fine");
         Console.WriteLine("Gotta gotta be down");
         Console.WriteLine("Because I want it all");
 
         // Unit Test #4
+    // Printing the fourth set of lyrics to the console
         Console.WriteLine("\nNow, this is a story all about how");
         Console.WriteLine("My life got flipped-turned upside down");
         Console.WriteLine("And I'd like to take a minute");
